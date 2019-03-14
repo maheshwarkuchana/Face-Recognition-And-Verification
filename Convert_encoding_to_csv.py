705f62a71cb1a5ab1549e03bf9c885b8664a4c0b
maheshwarkuchana <kmaheshwar1998@gmail.com>
2019-03-14 13:21:54 +0530

    1

--- Convert_encoding_to_csv.py
+++ Convert_encoding_to_csv.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 def main(encodings):
-    df1 = pd.read_csv("Excel_Data\\encode.csv")
+    df1 = pd.read_csv("Excel_Data\\encode1.csv")
     df1.drop(df1.columns[0],axis=1)
     for i in encodings:
-        i.insert(0, len(df1.index))
-        # print(type(i))
+        # i.insert(0, len(df1.index))
+        
         df1.loc[-1] = i
         df1.index = df1.index + 1 
         df1.sort_index()
         # df1.loc[len(df1.index)+1] = i
         # print(len(df1.index))
-        df1.to_csv("Excel_Data\\encode.csv",header=True)
+        df1.to_csv("Excel_Data\\encode1.csv",header=True)
     print(df1)
     print('Added Encoding')
