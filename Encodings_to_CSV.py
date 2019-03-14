b36a701d3cd78000577fad2092f9f63adc980997
maheshwarkuchana <kmaheshwar1998@gmail.com>
2019-03-15 00:12:23 +0530

    Enhanced using OOPS

---
+++ Encodings_to_CSV.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import pickle
+
+
+class Encodings_to_CSV:
+
+    df = pd.read_csv("Excel_Data\\encode.1.csv", index_col=0)
+
+    def __init__(self, df):
+        Encodings_to_CSV.df = df
+
+    def Update_Pickle():
+        filename = "Excel_Data\\Encodings.pickle"
+        pickle.dump(Encodings_to_CSV.df, open(filename, 'wb'))
+
+    def main(encodings):
+        print(Encodings_to_CSV.df.shape)
+
+        for i in encodings:
+            count = Encodings_to_CSV.df['name'].count()
+            Encodings_to_CSV.df.loc[count+1] = i
+            Encodings_to_CSV.df.to_csv("Excel_Data\\encode.1.csv", header=True)
+
+        print(Encodings_to_CSV.df.shape)
+        print('Added Encoding')
+        Encodings_to_CSV.Update_Pickle()
